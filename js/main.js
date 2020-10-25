c03459081a32001dff88838a8572cf6e573d0b1e
tamaki <hbk2002320@gmail.com>
2020-10-25 23:30:09 +0900

    add panel to main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 'use strict';
 {
+  class Panel {
+    constructor() {
+      this.el = document.createElement('li');
+      this.el.classList.add('pressed');
+    }
+
+    getEl() {
+      return this.el;
+    }
+  }
+
+
+  class Board {
+    constructor () {
+      this.panels = [];
+      for (let i = 0; i < 4; i++) {
+        this.panels.push(new Panel());
+      }
+      this.setup();
+    }
+
+    setup() {
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        board.appendChild(panel.getEl());  //カプセル化
+      });
+    }
+  }
   
+  const board = new Board();
 }
