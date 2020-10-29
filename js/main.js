41d0dddaf7127f35666ec73ab142de4de578dd50
tamaki <hbk2002320@gmail.com>
2020-10-29 21:43:38 +0900

    add gamelevel to js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@
         this.el.classList.add('pressed');
         this.game.addCurrentNum();
 
-        if (this.game.getCurrentNum() === 4) {
+        if (this.game.getCurrentNum() === this.game.getLevel() ** 2) {
           clearTimeout(this.game.getTimeoutId());
         }
       }
@@ -35,7 +35,7 @@
     constructor(game) {
       this.game = game;
       this.panels = [];
-      for (let i = 0; i < 4; i++) {
+      for (let i = 0; i < this.game.getLevel() ** 2; i++) {
         this.panels.push(new Panel(this.game));
       }
       this.setup();
@@ -49,7 +49,10 @@
     }
 
     activate() {
-      const nums = [0, 1, 2, 3];
+      const nums = [];
+      for (let i = 0; i < this.game.getLevel() ** 2; i++) {
+        nums.push(i);
+      }
 
       this.panels.forEach(panel => {
         const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
@@ -59,7 +62,8 @@
   }
 
   class Game {
-    constructor() {
+    constructor(level) {
+      this.level = level;
       this.board = new Board(this);
 
       this.currentNum = undefined;
@@ -70,6 +74,15 @@
       btn.addEventListener('click', () => {
         this.start();
       });
+      this.setup();
+    }
+
+    setup() {
+      const container = document.getElementById('container');
+      const PANEL_WIDTH = 50;
+      const BOARD_PADDING = 10;
+
+      container.style.width = PANEL_WIDTH * this.level + BOARD_PADDING * 2 + 'px';
     }
 
     start() {
@@ -104,7 +117,11 @@
     getTimeoutId() {
       return this.timeoutId;
     }
+
+    getLevel() {
+      return this.level;
+    }
   }
 
-  new Game();
+  new Game(3);
 }
